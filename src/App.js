11c0c3ba0350d7baa660936f7403578a067ada41
aaronktaey <aaronktaey@gmail.com>
2023-03-20 11:10:29 +0900

    initial

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <body>
+        Hello, MUI.
+      </body>
+    </div>
+  );
+}
+
+export default App;
